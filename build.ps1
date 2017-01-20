5e8414776a37003d4732528fa64fece7d79d2be3
Aaron Jensen <ajensen@webmd.net>
2017-01-20 14:51:02 -0800

    Make sure exit codes from other applications don't taint build result.

--- build.ps1
+++ build.ps1
@@ -20,6 +20,8 @@ param(
 
 Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'Pester' -Resolve)
 
+$Global:LASTEXITCODE = 0
+
 $result = Invoke-Pester -Script (Join-Path -Path $PSScriptRoot -ChildPath 'Tests' -Resolve) -OutputFormat NUnitXml -OutputFile (Join-Path -Path $PSScriptRoot -ChildPath 'pester.xml') -PassThru
 
 exit $result.FailedCount
