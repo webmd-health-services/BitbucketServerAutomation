ba51cf3a5dcc93a20576c68009de1cdb2e35239d
Scott Pickthorn <spickthorn@webmd.net>
2017-07-28 11:59:49 -0700

    adding initialize-BitbucketServerAutomationTest to functions

--- Tests/New-BBServerPullRequest.Tests.ps1
+++ Tests/New-BBServerPullRequest.Tests.ps1
@@ -9,7 +9,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+& (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-BitbucketServerAutomationTest.ps1' -Resolve)
 Set-StrictMode -Version 'Latest'
 
 $projectKey = 'NBBSBRANCH'
