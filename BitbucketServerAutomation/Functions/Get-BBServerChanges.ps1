21fe0fd550e15f4c2485de85722464771bcd1273
Scott Pickthorn <spickthorn@webmd.net>
2017-08-16 14:05:32 -0700

    adding Get-BBServerChanges function

---
+++ BitbucketServerAutomation/Functions/Get-BBServerChanges.ps1
@@ -0,0 +1,85 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function Get-BBServerChanges
+{
+    <#
+    .SYNOPSIS
+    Gets a list of branches from a repository.
+
+    .DESCRIPTION
+    The `Get-BBServerBranch` function returns a list of all branches in a Bitbucket Server repository.
+    
+    If you pass a branch name, the function will only return the information for the named branch and will return nothing if no branches are found that match the search criteria. Wildcards are allowed to search for files.
+
+    .EXAMPLE
+    Get-BBServerBranch -Connection $conn -ProjectKey 'TestProject' -RepoName 'TestRepo'
+
+    Demonstrates how to get the properties of all branches in the `TestRepo` repository.
+
+    .EXAMPLE
+    Get-BBServerBranch -Connection $conn -ProjectKey 'TestProject' -RepoName 'TestRepo' -BranchName 'master'
+
+    Demonstrates how to get the properties for the master branch in the `TestRepo` repository.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [object]
+        # An object that defines what Bitbucket Server to connect to and the credentials to use when connecting.
+        $Connection,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The key/ID that identifies the project where the repository resides. This is *not* the project name.
+        $ProjectKey,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The name of a specific repository.
+        $RepoName,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        # name of the source commit or Ref.
+        $From,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        # name of the target commit or Ref.
+        $To
+
+    )
+    Set-StrictMode -Version 'Latest'
+    $lastPage = $false
+    $nextPageStart = 0
+    $changes = $null
+    $changesList = @()
+
+    while ( -not $lastPage )
+    {
+        $ResourcePath = ('projects/{0}/repos/{1}/compare/changes?from={2}&to={3}' -f $ProjectKey, $RepoName, $From, $To)
+        $changes = Invoke-BBServerRestMethod -Connection $Connection -Method Get -ApiName 'api' -ResourcePath ('{0}&limit={1}&start={2}' -f $resourcePath, [int16]::MaxValue, $nextPageStart)
+        if( $changes)
+        {
+            $lastPage = $changes.isLastPage
+            $nextPageStart = $changes.nextPageStart
+            $changesList += $changes.values
+        }
+        else
+        {
+            return
+        }
+    }
+
+    return $changesList
+}
