0810492b9860f6c74c325e6d11f404d79efff5f0
Joseph Larionov <jlarionov@webmd.net>
2018-11-29 13:28:37 -0800

    Repository now builds and publishes using the Whiskey build platform

--- build.ps1
+++ build.ps1
@@ -1,6 +1,3 @@
-<#
-#>
-
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -12,40 +9,53 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-[CmdletBinding()]
+
+[CmdletBinding(DefaultParameterSetName='Build')]
 param(
+    [Parameter(Mandatory=$true,ParameterSetName='Clean')]
+    [Switch]
+    # Runs the build in clean mode, which removes any files, tools, packages created by previous builds.
+    $Clean,
+
+    [Parameter(Mandatory=$true,ParameterSetName='Initialize')]
+    [Switch]
+    # Initializes the repository.
+    $Initialize
 )
 
+
 #Requires -Version 4
-Set-StrictMode -Version 'Latest'
+Set-StrictMode -Version Latest
 
-.\init.ps1 -Verbose:$VerbosePreference
+& (Join-Path -Path $PSScriptRoot -ChildPath '.whiskey\Import-Whiskey.ps1' -Resolve)
 
-$originalVerbosePref = $VerbosePreference
+$configPath = Join-Path -Path $PSScriptRoot -ChildPath 'whiskey.yml' -Resolve
 
-$result = @()
-try
+$optionalArgs = @{ }
+if( $Clean )
 {
-    $VerbosePreference = 'SilentlyContinue'
-
-    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'Pester' -Resolve) -Verbose:$false -Force
-
-    $Global:LASTEXITCODE = 0
-
-    $result = Invoke-Pester -Script (Join-Path -Path $PSScriptRoot -ChildPath 'Tests' -Resolve) `
-                            -OutputFormat NUnitXml `
-                            -OutputFile (Join-Path -Path $PSScriptRoot -ChildPath 'pester.xml') `
-                            -PassThru `
-                            -Verbose:$false
-}
-finally
-{
-    $VerbosePreference = $originalVerbosePref
+    $optionalArgs['Clean'] = $true
 }
 
-$result | Format-List | Out-String | Write-Verbose
-
-if( $result.FailedCount )
+if( $Initialize )
 {
-    throw ('{0} tests failed.' -f $result.FailedCount)
+    $optionalArgs['Initialize'] = $true
 }
+
+$context = New-WhiskeyContext -Environment 'Dev' -ConfigurationPath $configPath
+
+$apiKeys = @{
+    'PowerShellGallery' = 'POWERSHELL_GALLERY_API_KEY'
+    'github.com' = 'GITHUB_ACCESS_TOKEN'
+}
+
+$apiKeys.Keys |
+    Where-Object { Test-Path -Path ('env:{0}' -f $apiKeys[$_]) } |
+    ForEach-Object {
+        $apiKeyID = $_
+        $envVarName = $apiKeys[$apiKeyID]
+        Write-Verbose ('Adding API key "{0}" with value from environment variable "{1}".' -f $apiKeyID,$envVarName)
+        Add-WhiskeyApiKey -Context $context -ID $apiKeyID -Value (Get-Item -Path ('env:{0}' -f $envVarName)).Value
+    }
+
+Invoke-WhiskeyBuild -Context $context @optionalArgs
