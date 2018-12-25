1c3efdf22ef65f765a93eae19c8175110e112aca
Joseph Larionov <jlarionov@webmd.net>
2018-12-24 16:42:17 -0800

    Add copyright information to all files with a license notice header
    The Apache License, Version 2.0 application instructions
    (https://www.apache.org/licenses/LICENSE-2.0#apply) state that the
    license notice header in each file should also include the copyright
    owner information.

--- Tests/LicenseNotices.Tests.ps1
+++ Tests/LicenseNotices.Tests.ps1
@@ -1,3 +1,5 @@
+# Copyright 2016 - 2018 WebMD Health Services
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -19,6 +21,9 @@ Describe 'License Notices' {
     $licenseFilePath = Join-Path -Path $projectRoot -ChildPath 'LICENSE' -Resolve
 
     $noticeLines = & {
+
+        'Copyright 2016 - {0} WebMD Health Services' -f (Get-Date).Year
+
         Get-Content -Path $licenseFilePath -Tail 11 |
         ForEach-Object {
             $line = $_.Trim()
